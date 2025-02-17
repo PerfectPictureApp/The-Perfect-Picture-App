685cf9a60fc455b23a6cc83792c0d8f4c6a3ac2b
Jessica Ellerbe <chiefellerbe@gmail.com>
2025-02-17 09:33:23 -0500

    Initial Commit

---
+++ The Perfect Picture App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Perfect Picture App
+//
+//  Created by Jessica Ellerbe on 2/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Perfect Picture App/The_Perfect_Picture_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Perfect_Picture_AppApp.swift
+//  The Perfect Picture App
+//
+//  Created by Jessica Ellerbe on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct The_Perfect_Picture_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
